7fd73e8bc9197f9263adb7d48a3d341a064f34ae
马润鹏 <marunpeng@run.local>
2018-05-19 13:23:45 +0800

    5

--- app.js
+++ app.js
@@ -1,64 +1,53 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
 
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-var xhub = require('express-x-hub');
+  app.get('/webhook', (req, res) => {
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "token"
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
 
-app.use(xhub({ algorithm: 'sha1', secret: "d7550e862b9f7713e37f3adc9ed516df" }));
-app.use(bodyParser.json());
+  app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    if (body.object === 'page') {
+  
+      body.entry.forEach(function(entry) {
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+         res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
 
-var token = 'token';
-var received_updates = [];
-
-app.get('/', function(req, res) {
-  console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
-});
-
-app.get(['/facebook', '/instagram'], function(req, res) {   
-  if (
-    req.param('hub.mode') == 'subscribe' &&
-    req.param('hub.verify_token') == token
-  ) {
-    res.send(req.param('hub.challenge'));
-  } else {
-    res.sendStatus(400);
-  }
-});
-
-app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
-
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
-
-  console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.listen(3000, function(){
-  console.log("success:" + 3000)
-});
+  app.listen(3000, () => {
+    console.log('webhook is listening')
+  })
