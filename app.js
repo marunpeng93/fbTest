c7e56c49109bd26531d3852408619f162f780592
马润鹏 <marunpeng@run.local>
2018-05-30 16:38:29 +0800

    test

--- app.js
+++ app.js
@@ -1,53 +1,14 @@
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json());
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-  app.get('/webhook', (req, res) => {
+app.set('port', 3000);
+app.use(bodyParser.json());
+app.use(cors());
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "token"
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
-    }
-  });
+app.listen(app.get('port'), function() {
+    console.log('server: ' , app.get('port'));
+});
 
-  app.post('/webhook', (req, res) => {  
- 
-    let body = req.body;
-    console.log(JSON.stringify(body) )
-    if (body.object === 'page') {
-  
-      body.entry.forEach(function(entry) {
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-      });
-         res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
-  
-  });
-
-  app.listen(3000, () => {
-    console.log('webhook is listening')
-  })
+require("./lib/bot")(app)
